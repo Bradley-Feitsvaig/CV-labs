5818a52477b121a4e9b832eda3c7bc37467df2cf
Bradley <bradley.feitsvaig@gmail.com>
2024-07-13 14:15:35 +0300

    founded and plotted matches

--- ex2/code_311183073_318239639.py
+++ ex2/code_311183073_318239639.py
@@ -3,6 +3,7 @@ import os
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+import random
 
 
 def load_data(image1_path, image2_path, k_matrix_path):
@@ -36,7 +37,7 @@ def find_key_points(image):
     :param image: Input image (np.ndarray).
     :return: Tuple with:
              - key_points: Detected key_points (tuple of cv2.KeyPoint).
-             - descriptors: Descriptors for the detected key_points (np.ndarray).
+             - descriptors: Descriptors for the detected key_points (np.ndarray (#key_points X 128 features)).
     """
     gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     sift = cv2.SIFT_create()
@@ -67,11 +68,73 @@ def plot_key_points(image1, key_points1, image2, key_points2):
     plt.show()
 
 
+def find_potential_matches(descriptors1, descriptors2):
+    """
+    Find potential matches between two key point descriptors of 2 images,
+    using BFMatcher.knnMatch() to get only 2 best matches for each descriptor.
+    Applying ratio test to filtering ambiguous matches, (uses threshold of 0.8).
+    :param descriptors1: Descriptors image1 key_points (np.ndarray (#key_points X 128 features))
+    :param descriptors2: Descriptors image2 key_points (np.ndarray (#key_points X 128 features))
+    :return: list of potential matches between descriptors1 and descriptors2.
+    """
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(descriptors1, descriptors2, k=2)
+
+    # Ratio test
+    good_matches = []
+    for m, n in matches:
+        if m.distance < 0.8 * n.distance:
+            good_matches.append([m])
+
+    return good_matches
+
+
+def get_matched_key_points(matches, key_points1, key_points2):
+    """
+    Get the matched key points.
+    :param matches: List of matches between 2 images.
+    :param key_points1: first image key_point (tuple of cv2.KeyPoint)
+    :param key_points2: second image key_point (tuple of cv2.KeyPoint)
+    :return: Tuple with:
+         - matched_key_points1: matched key_points from key_points1(tuple of cv2.KeyPoint).
+         - matched_key_points2: matched key_points from key_points2(tuple of cv2.KeyPoint).
+    """
+    matched_key_points1 = tuple(key_points1[m[0].queryIdx] for m in matches)
+    matched_key_points2 = tuple(key_points2[m[0].trainIdx] for m in matches)
+    return matched_key_points1, matched_key_points2
+
+
+def plot_matches(image1, key_points1, image2, key_points2, matches):
+    """
+    Plot the matches between two images.
+    :param image1: First image (np.ndarray).
+    :param key_points1: Key points of the first image (list of cv2.KeyPoint).
+    :param image2: Second image (np.ndarray).
+    :param key_points2: Key points of the second image (list of cv2.KeyPoint).
+    :param matches: List of matches (list of lists containing cv2.DMatch objects).
+    """
+    image_matches = cv2.drawMatchesKnn(image1, key_points1, image2, key_points2, matches, None,
+                                       matchColor=(0, 255, 255), flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+
+    plt.figure(figsize=(15, 7))
+    plt.imshow(cv2.cvtColor(image_matches, cv2.COLOR_BGR2RGB))
+    plt.title('Feature Matches')
+    plt.show()
+
+
 def main():
+    # Read images and calibration matrix.
     image1, image2, k = load_data('data/example_1/I1.png', 'data/example_1/I2.png', 'data/example_1/K.txt')
+    # Find key_points in 2 images and plot them.
     image1_key_points, image1_descriptors = find_key_points(image1)
     image2_key_points, image2_descriptors = find_key_points(image2)
     plot_key_points(image1, image1_key_points, image2, image2_key_points)
+    # Find matches between images and plot them.
+    matches = find_potential_matches(image1_descriptors, image2_descriptors)
+    matched_key_points1, matched_key_points2 = get_matched_key_points(matches, image1_key_points, image2_key_points)
+    plot_key_points(image1, matched_key_points1, image2, matched_key_points2)
+    random_matches_sample = random.sample(matches, k=70)
+    plot_matches(image1, image1_key_points, image2, image2_key_points, random_matches_sample)
 
 
 if __name__ == "__main__":
